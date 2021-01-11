38289e295343834e99865dd840880f62d7dfae71
annie wang <69293499+junyuwang801119@users.noreply.github.com>
2021-01-11 22:34:44 +0800

    Create 1.const.php

---
+++ 1.const.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>const</title>
+</head>
+
+<body>
+    <?php
+
+    define("myConst", 123);
+
+    echo myConst;
+
+
+
+
+    ?>
+</body>
+
+</html>
