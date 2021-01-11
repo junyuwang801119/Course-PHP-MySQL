bfd77b054f0a83229927cdac6b74edf0c659f3ad
annie wang <69293499+junyuwang801119@users.noreply.github.com>
2021-01-11 22:46:06 +0800

    Create 25.home test.php

---
+++ 25.home test.php
@@ -0,0 +1,13 @@
+<?php
+$page_title = 'Home Page';
+$page_name = 'dataList';
+require __DIR__ . '/part/__connect db.php';
+?>
+
+<?php require __DIR__ . '/part/__html header.php'; ?>
+<?php include __DIR__ . '/part/__navbar.php'; ?>
+<div class="container">
+    <h2>Hello</h2>
+</div>
+<?php include __DIR__ . '/part/__script.php'; ?>
+<?php include __DIR__ . '/part/__html footer.php'; ?>
