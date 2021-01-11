852e9f3d7602b396e4a36b2993f6bae8a897977c
annie wang <69293499+junyuwang801119@users.noreply.github.com>
2021-01-11 22:36:33 +0800

    Create 2.var.php

---
+++ 2.var.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>var</title>
+</head>
+
+<body>
+    <?php
+
+    echo empty($b) ? 'empty' : 'not empty';
+    echo '<br>';
+    echo !empty($b);
+    echo '<br>';
+    echo '$a<br>';
+    echo "$a<br>";
+    echo "\$a<br>";
+    echo "\n\"<br>";
+    echo '\n\" \'\\<br>
+123
+';
+
+    // $c = "bill";
+    // echo $c ?? 'peter';
+    echo '\n\" \'\\<br>123';
+
+    ?>
+</body>
+
+</html>
